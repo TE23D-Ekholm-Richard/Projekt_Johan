6845ebeed4c4b2e0b26b639e01efc9a7afbcc670
Richard Ekholm <richard.ekholm@elev.ga.ntig.se>
2026-05-11 11:02:14 +0200

    Börjar Barn klass nu

--- projekt_e/src/main/java/com/example/Library.java
+++ projekt_e/src/main/java/com/example/Library.java
@@ -1,7 +1,42 @@
 package com.example;
-
+//Detta är föräldrar klassen där barnklassen ärver från
 public class Library {
     private int id;
     private String titel;
      private boolean isAvailable;
+
+     public Library(int id, String titel, boolean isAvailable){
+        this.id=id;
+        this.titel=titel;
+        this.isAvailable=isAvailable;
+     }
+
+     public Library() {
+     }
+
+     public int getId() {
+         return id;
+     }
+
+     public void setId(int id) {
+         this.id = id;
+     }
+
+     public String getTitel() {
+         return titel;
+     }
+
+     public void setTitel(String titel) {
+         this.titel = titel;
+     }
+
+     public boolean isAvailable() {
+         return isAvailable;
+     }
+
+     public void setAvailable(boolean isAvailable) {
+         this.isAvailable = isAvailable;
+     }
+
+     
 }
