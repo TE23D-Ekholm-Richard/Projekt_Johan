8d21bcf656d4cc8f6605577bc32dd97a4d0565e4
Richard Ekholm <richard.ekholm@elev.ga.ntig.se>
2026-05-11 11:09:22 +0200

    klar för denna lektion

--- projekt_e/src/main/java/com/example/Book.java
+++ projekt_e/src/main/java/com/example/Book.java
@@ -1,5 +1,44 @@
 package com.example;
 
 public class Book extends Library {
+    private String author;
+    private String genre;
+    private int pages;
+
+    public Book(int id, String titel, boolean isAvailable, String author, String genre, int pages){
+        super(id,titel,isAvailable);
+        this.author=author;
+        this.genre=genre;
+        this.pages=pages;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    @Override
+    public String toString() {
+        return "Book [author=" + author + ", genre=" + genre + ", pages=" + pages + "]";
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
 
 }
