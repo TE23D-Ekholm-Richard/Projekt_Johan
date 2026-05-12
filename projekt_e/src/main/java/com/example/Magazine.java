0fb5be8051436a8e7b544a57a8066b6fc1222df4
Richard Ekholm <richard.ekholm@elev.ga.ntig.se>
2026-05-12 12:42:41 +0200

    E nivå

--- projekt_e/src/main/java/com/example/Magazine.java
+++ projekt_e/src/main/java/com/example/Magazine.java
@@ -1,5 +1,44 @@
 package com.example;
 
-public class Magazine {
+public class Magazine extends Library {
+    private int issueNumber;
+    private String category;
+    private int publishyear;
+
+    public Magazine(){
+
+    }
+
+    public Magazine(int id, String titel, boolean isAvailable, int issueNumber, String category, int publishyear){
+        super(id,titel,isAvailable);
+        this.issueNumber=issueNumber;
+        this.category=category;
+        this.publishyear=publishyear;
+
+    }
+
+    public int getIssueNumber() {
+        return issueNumber;
+    }
+
+    public void setIssueNumber(int issueNumber) {
+        this.issueNumber = issueNumber;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getPublishyear() {
+        return publishyear;
+    }
+
+    public void setPublishyear(int publishyear) {
+        this.publishyear = publishyear;
+    }
     
 }
